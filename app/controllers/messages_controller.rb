793293c17fe52be86c52cc17cf97504026fee370
Ben Braunstein <benzbraunstein@gmail.com>
2019-08-13 16:50:52 -0400

    first commit

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,11 @@
+class MessagesController < ApplicationController
+  def index
+    messages = Message.all
+    render :json => messages
+  end
+
+  def create
+    message = Message.create(sender: params[:sender], message: params[:message])
+    render :json => message
+  end
+end
